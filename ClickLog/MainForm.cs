349b0df78a6a06a9061ecebd4c471392af48bb97
Victor <publicdomainvic@gmail.com>
2022-05-04 20:30:21 -0400

    Add OnExitToolStripMenuItemClick()

--- ClickLog/MainForm.cs
+++ ClickLog/MainForm.cs
@@ -184,7 +184,8 @@ namespace ClickLog
         /// <param name="e">Event arguments.</param>
         private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Close program
+            this.Close();
         }
     }
 }
